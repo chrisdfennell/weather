e984d70523e21800ff9f2d2455ee9c262d24bb4f
Christopher Fennell <infantrykiller@gmail.com>
2025-06-08 14:14:36 -0400

    Initial commit: Set up project structure and base code

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Adjust to match your file structure
+    "./js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
